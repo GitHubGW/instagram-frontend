6ee511b75931c88bad5918f07cce85d59ecb1650
GW <kowonp@gmail.com>
2022-03-04 00:07:30 +0900

    Initialize ApolloClient

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { useReactiveVar } from "@apollo/client";
+import { ApolloProvider, useReactiveVar } from "@apollo/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
-import { isDarkModeVar, isLoggedInVar } from "./apollo";
+import client, { isDarkModeVar, isLoggedInVar } from "./apollo";
 import { darkTheme, lightTheme } from "./styles/themes";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -17,19 +17,21 @@ const App = () => {
   console.log("isLoggedIn", isLoggedIn);
 
   return (
-    <HelmetProvider>
-      <ThemeProvider theme={isDarkMode === true ? darkTheme : lightTheme}>
-        <GlobalStyle />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={isLoggedIn === true ? <Home /> : <Login />} />
-            {isLoggedIn === false && <Route path="/login" element={<Login />} />}
-            {isLoggedIn === false && <Route path="/signup" element={<SignUp />} />}
-            <Route path="/*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </ThemeProvider>
-    </HelmetProvider>
+    <ApolloProvider client={client}>
+      <HelmetProvider>
+        <ThemeProvider theme={isDarkMode === true ? darkTheme : lightTheme}>
+          <GlobalStyle />
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={isLoggedIn === true ? <Home /> : <Login />} />
+              {isLoggedIn === false && <Route path="/login" element={<Login />} />}
+              {isLoggedIn === false && <Route path="/signup" element={<SignUp />} />}
+              <Route path="/*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </ThemeProvider>
+      </HelmetProvider>
+    </ApolloProvider>
   );
 };
 
--- src/apollo.ts
+++ src/apollo.ts
@@ -1,3 +1,12 @@
-import { makeVar } from "@apollo/client";
+import { ApolloClient, InMemoryCache, makeVar, NormalizedCacheObject } from "@apollo/client";
 
 export const isLoggedInVar = makeVar<boolean>(false);
+
+export const isDarkModeVar = makeVar<boolean>(false);
+
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
+});
+
+export default client;
