daf0edcd794b42739c5c20b1e2919c6a56f57025
GW <kowonp@gmail.com>
2022-03-07 00:41:41 +0900

    Avatar shared

---
+++ src/shared/Avatar.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+interface AvatarProps {
+  size: string;
+  avatarUrl?: string | null | undefined;
+}
+
+const Image = styled.img<{ size: string }>`
+  width: ${(props) => props.size};
+  height: ${(props) => props.size};
+  border: 1px solid ${(props) => props.theme.borderColor};
+  border-radius: 50%;
+`;
+
+const Avatar = ({ size, avatarUrl }: AvatarProps) => {
+  return <Image size={size} src={avatarUrl || ""} alt="" />;
+};
+
+export default Avatar;
